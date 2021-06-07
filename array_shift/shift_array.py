ba29be92075e82ab8df37d6b7429e23ef03bf759
bayan-alkhatib <bayankhatibtr@gmail.com>
2021-06-07 23:42:16 +0300

    complete challenge 2

--- array_shift/shift_array.py
+++ array_shift/shift_array.py
@@ -1,7 +1,7 @@
 list=[2,5,9,6,8,4,1]
 val= 7
 
-def shifted_arr(arr,val):
+def insertShiftArray(arr,val):
     new_array=[0 for i in range (len(arr)+1)]
     for i in range (len(arr)+1):
       if i == len(arr)//2:
@@ -12,4 +12,4 @@ def shifted_arr(arr,val):
           new_array[i]=arr[i]
     print(new_array)
 
-shifted_arr(list,val)
+insertShiftArray(list,val)
