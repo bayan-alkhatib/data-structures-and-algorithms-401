d62643fd64519025b96da3ba60167ca9a340f58c
bayan-alkhatib <bayankhatibtr@gmail.com>
2021-06-07 21:08:47 +0300

    add another code

--- array_reverse/array.py
+++ array_reverse/array.py
@@ -1,10 +1,18 @@
 list=[2,5,9,6]
 
+# def revers(arr):
+#     new_array=[]
+#     for i in range(len(arr)-1,-1,-1):
+#         print(i)
+#         new_array.append(arr[i])
+#     print(new_array)
+
+# revers(list)
+
 def revers(arr):
-    new_array=[]
-    for i in range(len(arr)-1,-1,-1):
-        print(i)
-        new_array.append(arr[i])
+    new_array=[0 for i in arr ]
+    for i in range(0,len(arr)):
+        new_array[i]=arr[len(arr)-1-i]
     print(new_array)
 
 revers(list)
